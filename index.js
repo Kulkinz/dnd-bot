5e3fe6e742386199bbc9a8502f5e7a2c2f322d71
Kulkinz <cool@coolspages.com>
2019-09-23 20:11:52 -0500

    Added base for proper command selection, and cleaned up dice logic

--- index.js
+++ index.js
@@ -10,9 +10,16 @@ client.once("ready", () => {
 
 // Runs when message is sent
 client.on('message', message => {
-	if (message.content === "%d20") {
+
+	// Gets the prefix from the config file. {"prefix":"prefix"}
+	const prefix = config.prefix;
+	// Removes the prefix from message, and then shifts to lowercase
+	const args = message.content.slice(prefix.length).trim().split(/ +/g);
+	const command = args.shift().toLowerCase();
+
+	if (command === "d20") {
 		message.channel.send("Rolling a d20.");
-		var value = Math.floor(Math.random() * (+20 - +1)) + +1;
+		var value = Math.floor(Math.random() * (20)) + 1;
 		message.channel.send(value);
 		console.log(value);
 
